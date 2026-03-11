b3fbc75d07de6c5d1d28f9c7978584f098c747c4
7ames <123058@combertonvc.org>
2026-03-11 13:44:06 +0000

    Update rand-int.js

--- functions/rand-int.js
+++ functions/rand-int.js
@@ -1,7 +1,7 @@
 export async function onRequest() {
   // Generate a random integer
   const randomInt = Math.floor(Math.random() * 10) + "\n";
-  return new Response(randomInt, {
+  return new Response("k| "+ randomInt, {
     headers: { 'Content-Type': 'text/plain' }
   });
 }
