7cea452bf5ff383237c4c057a3688fc4f0d0fbb4
7ames <123058@combertonvc.org>
2026-03-11 13:44:27 +0000

    Update timestamp.js

--- functions/timestamp.js
+++ functions/timestamp.js
@@ -1,6 +1,6 @@
 export async function onRequest() {
   const timestamp = Math.floor(Date.now() / 1000) + "\n";
-  return new Response(timestamp.toString(), {
+  return new Response("k| "+ timestamp.toString(), {
     headers: { 'Content-Type': 'text/plain' }
   });
 }
