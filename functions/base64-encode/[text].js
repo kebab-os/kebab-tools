88ce7aa6848eca3d9be035413bd0c42b36e217d9
7ames <123058@combertonvc.org>
2026-03-11 13:28:45 +0000

    Update [text].js

--- functions/base64-encode/[text].js
+++ functions/base64-encode/[text].js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
   const { text } = context.params;
-  const encoded = btoa(text) + "\n";
+  const encoded = "| " + btoa(text) + "\n";
   
   return new Response(encoded, {
     headers: { 'Content-Type': 'text/plain' }
