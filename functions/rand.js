f92835441af58fbd0915199dec064734bea0e5af
7ames <123058@combertonvc.org>
2026-03-11 13:44:20 +0000

    Update rand.js

--- functions/rand.js
+++ functions/rand.js
@@ -1,7 +1,7 @@
 export async function onRequest() {
   // Generate a random number
   const random = Math.random() + "\n";
-  return new Response(random, {
+  return new Response("k| "+ random, {
     headers: { 'Content-Type': 'text/plain' }
   });
 }
