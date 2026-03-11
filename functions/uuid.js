8fd8dc0510f34503d7b1520964c692755c272f34
7ames <123058@combertonvc.org>
2026-03-11 13:44:57 +0000

    Update uuid.js

--- functions/uuid.js
+++ functions/uuid.js
@@ -1,7 +1,7 @@
 export async function onRequest() {
   // Generate a random UUID v4
   const uuid = crypto.randomUUID() + "\n";
-  return new Response(uuid, {
+  return new Response("k| "+ uuid, {
     headers: { 'Content-Type': 'text/plain' }
   });
 }
