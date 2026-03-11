2e75cb6b8dcc1a2046da011b0210d0244989e0b8
7ames <123058@combertonvc.org>
2026-03-11 10:27:14 +0000

    Create password-gen.js

---
+++ functions/password-gen.js
@@ -0,0 +1,15 @@
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  const length = parseInt(url.searchParams.get('len')) || 16;
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+  
+  let password = "";
+  const array = new Uint32Array(length);
+  crypto.getRandomValues(array);
+  
+  for (let i = 0; i < length; i++) {
+    password += charset[array[i] % charset.length];
+  }
+
+  return new Response(password);
+}
