b60a3e142154078d2621cb03e9a4f114f4772f2c
7ames <123058@combertonvc.org>
2026-03-12 08:34:56 +0000

    Update user-agent.js

--- functions/user-agent.js
+++ functions/user-agent.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
   const ua = context.request.headers.get('user-agent');
-  return new Response("k| "+ JSON.stringify({ user_agent: ua }, null, 2), {
+  return new Response("k|\n"+ JSON.stringify({ user_agent: ua }, null, 2) + "\n", {
     headers: { 'content-type': 'application/json' }
   });
 }
