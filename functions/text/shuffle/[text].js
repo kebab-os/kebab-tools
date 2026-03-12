b23c4ec4db628b1acfbe67652d45d3073f539abc
7ames <123058@combertonvc.org>
2026-03-12 09:41:04 +0000

    Update [text].js

--- functions/text/shuffle/[text].js
+++ functions/text/shuffle/[text].js
@@ -1,1 +1,11 @@
-
+export async function onRequest(context) {
+  const { text } = context.params;
+  const arr = text.split('');
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return new Response("k| " + arr.join('') + "\n", {
+    headers: { 'Content-Type': 'text/plain' }
+  });
+}
