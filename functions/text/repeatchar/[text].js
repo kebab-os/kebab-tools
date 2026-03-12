2703b3d02520a98023928be5e207f15e05d5cec1
7ames <123058@combertonvc.org>
2026-03-12 10:00:36 +0000

    Create [text].js

---
+++ functions/text/repeatchar/[text].js
@@ -0,0 +1,9 @@
+// repeatchar/[text].js
+export async function onRequest(context) {
+  const { text } = context.params;
+  const [char, times] = text.split(',');
+  const repeated = (char || ' ').repeat(Math.min(parseInt(times) || 1, 100));
+  return new Response("k| " + repeated + "\n", {
+    headers: { 'Content-Type': 'text/plain' }
+  });
+}
