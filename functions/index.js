ddfc6775a3367c7b4edcd1d4240bbdfc0ccd1703
7ames <123058@combertonvc.org>
2026-03-17 10:23:39 +0000

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,18 @@
 export async function onRequest() {
   const message = `
-k| kebab-tools
- | ================
- | 
- | For full list of tools, read /list\n
+| kebab-tools
+| ========================================
+| 
+| Welcome to kebab-tools! Use this to
+| easily access tools straight from your
+| terminal, or the web.
+| 
+| Example usage:
+| curl https://tools.kebabos.me/random/int
+| 
+| For full list of tools, read /list\n
+| 
+|                       © kebab-tools 2026
 `;
   
   return new Response(message, {
