fd05b0810721d110a13bf1c3f75d73e8007b0d5b
7ames <123058@combertonvc.org>
2026-03-18 12:57:58 +0000

    Revise help message for kebab-tools

--- functions/help.js
+++ functions/help.js
@@ -1,22 +1,22 @@
 export async function onRequest() {
   const message = `
-| HELP | kebab-tools
-| ========================================
-| 
-| kebab-tools is a tool app hosted on
-| cloudfare pages. It can be used on
-| terminal (e.g. using curl) or from
-| browser.
-| 
-| The URL is 'https://tools.kebabos.me',
-| and endpoints follow the pattern:
-| 'https://tools.kebabos.me/random/int'.
-| 
-| Using a browser is just the same as using
-| a terminal to access kebab-tools.
-| 
-| Report any issues at:
-| https://github.com/kebab-os/kebab-tools/issues\n
+HELP | kebab-tools
+========================================
+
+Kebab-tools is a versatile suite of command-line utilities designed for seamless integration into your terminal workflow via curl. Built for developers who value efficiency and minimalism, it eliminates the need to switch between windows or leave the CLI to perform common tasks. Whether you're debugging, formatting data, or managing system operations, kebab-tools provides a fast, dependency-free way to access essential developer resources directly from your shell.
+
+To use kebab-tools, curl the endpoint for the tool that you wish to use like this: 'curl https://tools.kebabos.me/random/int'.
+
+Kebab-tools is a tool app hosted on cloudfare pages, using cloudfare pages functions: 'https://developers.cloudflare.com/pages/functions'.
+
+The URL is 'https://tools.kebabos.me', and endpoints follow the pattern: 'https://tools.kebabos.me/dir/name', 'https://tools.kebabos.me/dir/name/text' or 'https://tools.kebabos.me/dir/subdir/name'.
+
+To view a json visualisation for the list of all the tools, curl 'https://tools.kebabos.me/list'.
+
+Using a browser is just the same as using a terminal to access kebab-tools.
+
+Report any issues at:
+https://github.com/kebab-os/kebab-tools/issues\n
 `;
   
   return new Response(message, {
