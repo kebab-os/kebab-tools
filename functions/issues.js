2056a8e20cc94ef146854b758c09d5be546ecfa3
7ames <123058@combertonvc.org>
2026-03-18 12:57:39 +0000

    Add onRequest function to handle issue reporting

--- functions/issues.js
+++ functions/issues.js
@@ -1,2 +1,10 @@
+export async function onRequest() {
+  const message = `
 Report any issues at:
-https://github.com/kebab-os/kebab-tools/issues\n
+https://github.com/kebab-os/kebab-tools/issues\N
+`;
+  
+  return new Response(message, {
+    headers: { 'Content-Type': 'text/plain' }
+  });
+}
