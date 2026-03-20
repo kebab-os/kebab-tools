bf3eaa54f0f33b5591594f757c410dae42bf51c3
7ames <123058@combertonvc.org>
2026-03-20 10:24:43 +0000

    Update client-shell-install.sh

--- install/client-shell-install.sh
+++ install/client-shell-install.sh
@@ -1,4 +1,4 @@
 cd ~
 mkdir kt
 cd kt
-curl -O https://github.com/kebab-os/kebab-tools/blob/83e4fb1e55f81bc784baf160fed5b34e7f07065a/cs/kebab-tools.py
+curl -O https://raw.githubusercontent.com/kebab-os/kebab-tools/refs/heads/main/cs/kebab-tools.py
