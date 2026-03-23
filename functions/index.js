4e7fa150714bdef83d11e3d9cdb29dc7da605c77
7ames <123058@combertonvc.org>
2026-03-23 11:31:53 +0000

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,14 @@
-export async function onRequest() {
+export async function onRequest(context) {
+  const { request } = context;
+  const accept = request.headers.get("accept") || "";
+
+  // If the requester prefers HTML (standard for browsers), redirect them
+  if (accept.includes("text/html")) {
+    const url = new URL(request.url);
+    return Response.redirect(`${url.origin}/web.html`, 302);
+  }
+
+  // Otherwise, serve the CLI message
   const message = `
 HOME | kebab-tools
 ========================================
@@ -12,7 +22,7 @@ For full list of tools, read '/list'
 ---
 kebab-tools 2026\n
 `;
-  
+
   return new Response(message, {
     headers: { 'Content-Type': 'text/plain' }
   });
