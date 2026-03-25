a0527716333b54f9314819576fbeab44c32f8ee8
7ames <123058@combertonvc.org>
2026-03-25 12:20:21 +0000

    Create whoami.js

---
+++ functions/misc/whoami.js
@@ -0,0 +1,17 @@
+export async function onRequest(context) {
+  const { request } = context;
+  const cf = request.cf;
+  const data = {
+    ip: request.headers.get('cf-connecting-ip'),
+    country: cf.country,
+    city: cf.city,
+    timezone: cf.timezone,
+    colo: cf.colo // Data centre code
+  };
+  
+  const output = JSON.stringify(data, null, 2) + "\n";
+  
+  return new Response(output, {
+    headers: { 'content-type': 'application/json' }
+  });
+}
