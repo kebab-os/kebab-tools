6e5dc1b3e78c3a34135902937a483e0950890b03
7ames <123058@combertonvc.org>
2026-03-25 12:15:40 +0000

    Add onRequest function to return current time as string

---
+++ functions/time/string.js
@@ -0,0 +1,7 @@
+export async function onRequest() {
+  // String of current time
+  date = Date.now().toString()
+  return new Response(date, {
+    headers: { 'Content-Type': 'text/plain' }
+  });
+}
